7009c3f99c3eb54c5c2a2ed165d679146c891916
sarameeo <sarameo@icloud.com>
2024-03-07 15:30:19 +0100

    Modified comment

--- src/main/java/provagit/mainclass.java
+++ src/main/java/provagit/mainclass.java
@@ -6,7 +6,8 @@ public class mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("A Message");
+		
+		System.out.println("This is A Message");
 
 	}
 
